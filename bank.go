dda11308506a9beffd88b60f4b03001dfa6c365d
Kuzmin <ar.kuzmin@bftcom.com>
2024-03-06 21:51:00 +0300

    test commit

--- bank.go
+++ bank.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
+	// git test
+	// hello
 
 	fmt.Println("Welcom to go Bank!")
 	fmt.Println("What you want to do?")
@@ -10,5 +12,9 @@ func main() {
 	fmt.Println("2. Deposit money")
 	fmt.Println("3. Withdrow money")
 	fmt.Println("4. Exit")
+
+	var choice int
+
+	fmt.Scan(&choice)
 
 }
